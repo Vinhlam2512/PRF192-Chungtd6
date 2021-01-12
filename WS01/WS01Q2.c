d9fad4d8eae42d75120a4e074bd00d32c79e768d
Vinhlam2512 <lamvinh2431@gmail.com>
2021-01-12 23:43:40 +0700

    1

--- WS01/WS01Q2.c
+++ WS01/WS01Q2.c
@@ -3,13 +3,42 @@
 #include<math.h>
 
 int main(){
-	int a;
-	char c;
-	float f, f2;
-	a = 50; c = 'f'; //f = 3.2
-	a = a*a;
-	scanf("%f %f",&f , &f2);
-	printf("%d %c\n", a, c);
-	printf("%f\n", f2);
-	return 0;
+	double a, b, c;
+	scanf("%lf %lf %lf", &a, &b, &c);
+	double min = a, max = c;
+	if(min > b){
+		min = b;
+		if(c < min){
+			printf("\nMin = %lf", c);
+		}else{
+			printf("\nMin = %lf", b);
+		}
+	}else if(min > c){
+		min = c;
+		if( b < min){
+			printf("\nMin = %lf", b);
+		}else{
+			printf("\nMin = %lf", c);
+		}
+	}else{
+		printf("\nMin = %lf", a);
+	}
+	if(max < b){
+		max = b;
+		if(max < a){
+			printf("\nMax = %lf", a);
+		}else{
+			printf("\nMax = %lf", b);
+		}
+	}else if(max < a){
+		max = a;
+		if(max < b){
+			printf("\nMax = %lf", b);
+		}else{
+			printf("\nMax = %lf", a);
+		}
+	}else{
+		printf("\nMax = %lf", c);
+	}
+	
 }
